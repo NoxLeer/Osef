80498e36d6e45a277214fbcc7887aae9d6ee043e
Jordan <hereng.jordan@outlook.fr>
2022-10-20 17:24:18 +0200

    je vous présente geoguessr

--- js/meteo.js
+++ js/meteo.js
@@ -5,5 +5,3 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 2,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-
